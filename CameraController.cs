81e15d49be88ab6d56970b45039f01959404abfd
Ming Huang <humorean@gmail.com>
2020-06-19 17:55:52 -0700

    player setup

---
+++ CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Player player;
+    private Vector3 lastPosition;
+    private float distance;
+    private float distanceY;
+    // Start is called before the first frame update
+    void Start()
+    {
+      player = FindObjectOfType<Player>(); // get last position of player
+      lastPosition = player.transform.position;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = player.transform.position.x - lastPosition.x;
+        distanceY = player.transform.position.y - lastPosition.y;
+        transform.position= new Vector3(
+            transform.position.x + distance,
+            transform.position.y + distanceY,
+            transform.position.z
+        );
+        lastPosition = player.transform.position;
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>(); //GetComponent is refer to the Player Class
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x+speed, rigidBody.velocity.y);
+        if(Input.GetKey("d") && rigidBody.velocity.x<3) {
+            Debug.Log(rigidBody.velocity.x);
+
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x+1, rigidBody.velocity.y);
+        }
+        if(Input.GetKey("a") && rigidBody.velocity.x>-3) {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x-1, rigidBody.velocity.y);
+        }
+        
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)){
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jump);
+        }
+    }
+}
